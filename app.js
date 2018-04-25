3003ab4b4ebd32aa92fe894ee77b13950e64177c
cantdocpp <cihuyman95@gmail.com>
2018-04-25 20:35:37 +0700

    terakhir session

--- app.js
+++ app.js
@@ -2,23 +2,32 @@ const express = require('express')
 const app = express();
 var bodyParser = require('body-parser');
 var session = require('express-session')
+const authMiddleware = require('./middlewares/authMiddleware');
 
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+// app.locals.isLoggedIn = require('./helpers/checkIsLogin');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-//router index
-var routerIndex = require('./routes');
-app.use('/', routerIndex);
-
-//router login
-var routerLogin = require('./routes/login');
-app.use('/login', routerLogin);
 
 //register
 var routerregister = require('./routes/register');
 app.use('/register', routerregister);
 
+//router login
+var routerLogin = require('./routes/login');
+app.use('/login', routerLogin);
+
+app.use('/', authMiddleware)
+
+//router index
+var routerIndex = require('./routes');
+app.use('/', routerIndex);
 
 app.listen(3000, () => console.log('app is listening on port 3000!'))
