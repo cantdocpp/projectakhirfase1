785272f28a5fea35ec036272e04e4bc78a4f12fb
cantdocpp <cihuyman95@gmail.com>
2018-04-25 14:16:56 +0700

    buat form login

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express();
+var bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs');
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
+//router index
+var routerIndex = require('./routes');
+app.use('/', routerIndex);
 
 //router login
 var routerLogin = require('./routes/login');
-app.get('/login', routerLogin)
-
-//router index
-var routerindex = require('./routes');
-app.get('/', routerindex)
+app.use('/login', routerLogin);
 
 
 app.listen(3000, () => console.log('app is listening on port 3000!'))
